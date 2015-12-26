c0eeecc3cf467039c4a03de98f19c5e844984a6e
Matti Jokitulppo <melonmanchan@gmail.com>
2015-12-26 17:55:41 +0200

    Don't start if no secret provided

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    SECRET:      process.env.SECRET                  || '',
+    SECRET:      process.env.SECRET                  || undefined,
     HOOK_PATH:   process.env.HOOK_PATH               || '/',
     PORT:        process.env.PORT                    || 8080,
     BRANCH:      'refs/heads/' + (process.env.BRANCH || 'master')
